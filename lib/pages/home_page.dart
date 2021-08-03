03488ee251d7005c3f78e44d702870cffcbca506
Ankur Rastogi <to.ankur21@gmail.com>
2021-08-03 16:25:33 +0530

    ListView builder, List generate & Card

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item-widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(21, (index) => CatalogModel.items[0]);
     int days = 30;
     return Scaffold(
       appBar: AppBar(
@@ -20,9 +23,15 @@ class HomePage extends StatelessWidget {
           // ),
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to flutter!!! $days"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
